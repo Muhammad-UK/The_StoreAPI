0f22bc7891b5e0db8c0b0b0bb3c2febb8a4224c4
Muhammad-UK <muhammadkhan3655@gmail.com>
2024-02-26 14:20:45 -0600

    started work on frontend, fetching users and products

---
+++ client/README.md
@@ -0,0 +1,9 @@
+# Front-end Client for TheStore API
+
+### Built using Vite, React, Tailwind, and Typescript
+
+### Just a simple test website to test the RESTful API routes:
+
+`GET`
+`DELETE`
+`POST`
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Favorite, Product, User } from "../../server/src/types";
+import { fetchProducts } from "../../server/src/db";
 
 function App() {
-  return <></>;
+  const [users, setUsers] = useState<User[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [favorites, setFavorites] = useState<Favorite[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("/api/users");
+      const json = await response.json();
+      setUsers(json as User[]);
+    };
+    fetchUsers();
+  }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("/api/products");
+      const json = await response.json();
+      setProducts(json as Product[]);
+    };
+    fetchProducts();
+  }, []);
+  // useEffect(() => {
+  //   const fetchFavorites = async () => {
+  //     const response = await fetch("/api/favorites");
+  //     const json = await response.json();
+  //     setFavorites(json as Favorite[]);
+  //   };
+  //   fetchFavorites();
+  // }, []);
+  return (
+    <div className="p-4 flex flex-wrap gap-56 mb-6">
+      <div className="space-y-2">
+        <h1 className="border-violet-700 border-solid border-4 p-4">Users</h1>
+        <ul className="border-violet-700 border-solid border-2 p-2">
+          {users.map((user) => {
+            return <li key={user.id}>{user.username}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="space-y-2">
+        <h1 className="border-pink-600 border-solid border-4 p-4">Products</h1>
+        <ul className="border-pink-600 border-solid border-2 p-2">
+          {products.map((product) => {
+            return <li key={product.id}>{product.name}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
